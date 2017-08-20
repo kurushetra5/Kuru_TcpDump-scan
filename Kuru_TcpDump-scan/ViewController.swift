d260e00730b9ae1e8ad13f0a07415d986eced3fa
Kurushetra <kurushetra@iMac-de-Kurushetra.local>
2017-08-20 17:46:37 +0200

    Initial Commit

---
+++ Kuru_TcpDump-scan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Kuru_TcpDump-scan
+//
+//  Created by Kurushetra on 20/8/17.
+//  Copyright © 2017 Kurushetra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
